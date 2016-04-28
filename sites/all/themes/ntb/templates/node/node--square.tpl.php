3144a8bb3284b257f48c0edc88659c3888779129
Gennaro Avolio-Toly <gennaro.the.younger@gmail.com>
2016-04-28 16:57:33 -0700

    Theme updates

---
+++ sites/all/themes/ntb/templates/node/node--square.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Square Tile view mode node template.
+ */
+?>
+<div class="tile <?php print $type; ?>">
+  <?php if (isset($image)): ?>
+    <div class="img">
+      <?php print render($image); ?>
+    </div>
+  <?php endif; ?>
+  <div class="title">
+    <h3>
+      <?php if (isset($title)): ?>
+        <?php print l($title, $node_url, array('html' => TRUE)); ?>
+      <?php else: ?>
+        <?php print $title; ?>
+      <?php endif; ?>
+    </h3>
+  </div>
+</div>
