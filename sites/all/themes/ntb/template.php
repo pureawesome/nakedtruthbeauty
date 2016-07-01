ef00377e25f0450e196de4cdb76225ec2f613d53
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-07-01 15:31:17 -0700

    features_extra, theming

--- sites/all/themes/ntb/template.php
+++ sites/all/themes/ntb/template.php
@@ -6,3 +6,20 @@
 function ntb_preprocess_page(&$variables) {
   drupal_add_js(libraries_get_path('bootstrap') . '/js/bootstrap.min.js');
 }
+
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function ntb_preprocess_form_element(&$vars) {
+  // $element = $vars['element'];
+  // if ($element['#type'] == 'textfield') {
+  //   $vars['element']['#attributes']['class'][] = 'form-control';
+  // }
+}
+
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function ntb_theme_form_element(&$vars) {
+
+}
