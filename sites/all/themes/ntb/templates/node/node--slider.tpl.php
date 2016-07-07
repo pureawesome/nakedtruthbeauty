f023eef1dbfe017eb11869d6668238b0743fb013
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-07-06 20:33:36 -0700

    marquee and ingredients

--- sites/all/themes/ntb/templates/node/node--slider.tpl.php
+++ sites/all/themes/ntb/templates/node/node--slider.tpl.php
@@ -12,7 +12,7 @@
   </a>
   <div class="content panel-panel panel-col col-sm-6" style="background: <?php if (isset($hex0)): ?><?php print $hex0; ?><?php endif; ?>">
     <h3 class='title'<?php if (isset($hex1)): ?> style="color: <?php print $hex1; ?>"<?php endif; ?>>
-      <?php print l($title, $link, array('html' => FALSE)); ?>
+      <?php print l($title, $link, array('html' => TRUE)); ?>
     </h3>
     <div class="marquee-body"<?php if (isset($hex2)): ?> style="<?php print $hex2; ?>"<?php endif; ?>>
       <?php print render($content['body']); ?>
