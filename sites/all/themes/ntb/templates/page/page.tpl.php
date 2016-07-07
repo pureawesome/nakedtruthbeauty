49fb23ad621ad162876f993ec77349334d2b0428
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-07-07 14:18:17 -0700

    cleanup space

--- sites/all/themes/ntb/templates/page/page.tpl.php
+++ sites/all/themes/ntb/templates/page/page.tpl.php
@@ -142,8 +142,6 @@
         </div>
       </div> <!-- /.section, /#navigation -->
 
-
-
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
