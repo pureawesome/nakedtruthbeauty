7c2b0bdf36faa337330a84164c672ef863656742
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-08-19 09:24:52 -0700

    fix js for products and shop

--- sites/all/modules/custom/features/site_products/js/site_products.js
+++ sites/all/modules/custom/features/site_products/js/site_products.js
@@ -8,27 +8,17 @@
   Drupal.behaviors.site_products = {
     attach: function (context, settings) {
       var self = this;
-      $('.view-shop').once('same-columns', function () {
-        // $(window).on('resize orientationchange', $.debounce(500, self.resizeColumns));
-        setTimeout(function () {
-          self.resizeColumns();
-        }, 1000);
-      });
 
       $('.node-type-product-display').once('tabs', function () {
         self.enableTabs();
-      })
+      });
     },
 
-    enableTabs: function() {
+    enableTabs: function () {
       $('.product-body .nav-tabs a').on('click', function (event) {
         event.preventDefault();
         $(this).tab('show');
       });
-    },
-
-    resizeColumns: function () {
-      // $('.view-shop .views-row').matchHeight();
     }
   };
 })(jQuery);
