81cc3294fbc16fcd66cc1e43c0fe17113f2ca623
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-08-31 15:27:02 -0700

    lazy load marquees

---
+++ sites/all/modules/custom/features/site_marquee/js/site_marquee.js
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * Javascript for site_marquee module.
+ */
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.site_marquee = {
+    attach: function (context, settings) {
+      var self = this;
+
+      $('.view-marquee .flexslider').once('tabs', function () {
+        // var $slider = $(this).data('flexslider', true);
+        // $slider.on('start', self.start($slider));
+        self.loadImg(this);
+      });
+    },
+
+    loadImg: function (slider) {
+      var $slides = $(slider).find('.slides').children();
+      $slides.each(function () {
+        var $image = $('.slider-image', this);
+        var img_url = $image.attr('data-img');
+        var preload_image = new Image();
+        preload_image.src = img_url;
+        preload_image.onload = function () {
+          var css_string = 'url(' + img_url + ')';
+          $image.css('background-image', css_string);
+          $image.removeClass('fa fa-spin fa-spinner fa-2x');
+        };
+      });
+    }
+  };
+})(jQuery);
