0a981673498b413eadbd020e66c2aa69af477314
Gennaro Avolio-Toly <gatman_toly@pureawesome.com>
2017-01-25 22:22:09 -0800

    Marquee idea (#30)

--- sites/all/themes/ntb/templates/page/page.tpl.php
+++ sites/all/themes/ntb/templates/page/page.tpl.php
@@ -202,6 +202,8 @@
       </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
 
+    <?php print render($page['marquee']); ?>
+
     <?php print $messages; ?>
 
     <div id="main-wrapper"><div id="main" class="clearfix">
@@ -209,19 +211,18 @@
       <div id="content" class="column"><div class="section">
         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
         <a id="main-content"></a>
-        <div class="container-fluid">
-          <div class="row">
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?><h1 class="title col-sm-12" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-            <?php print render($title_suffix); ?>
-          </div>
-        </div>
 
         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
         <div class="container-fluid">
+          <div class="row">
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?><h1 class="title col-sm-12" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+            <?php print render($title_suffix); ?>
+          </div>
+
           <div class="row">
             <div class="col-sm-12">
               <?php print render($page['content']); ?>
