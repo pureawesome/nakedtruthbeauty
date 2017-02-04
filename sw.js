45b2436d874295812fb9c84e957780a285bd64a5
Gennaro Avolio-Toly <gennaroavolio-toly@Gennaros-MacBook-Pro.local>
2017-02-04 10:32:47 -0800

    sw updates

--- sw.js
+++ sw.js
@@ -2,9 +2,6 @@ var CACHE_NAME = 'ntb-cache-v1';
 var urlsToCache = [
   '/',
   '/sites/all/themes/ntb/logo.png',
-  '/shop',
-  '/about',
-  '/ingredients',
   '/sites/all/themes/ntb/fonts/quicksand-regular-webfont.woff2',
   '/sites/all/themes/ntb/fonts/fontawesome-webfont.woff2?v=4.6.3',
   '/sites/all/themes/ntb/fonts/have_heart_one-webfont.woff2',
@@ -12,9 +9,14 @@ var urlsToCache = [
   '/misc/jquery.once.js?v=1.2',
   '/misc/drupal.js',
   '/misc/ajax.js?v=7.53',
-  '/misc/progress.js?v=7.53'
-  // '/styles/main.css',
-  // '/script/main.js'
+  '/misc/progress.js?v=7.53',
+  // '/sites/all/modules/custom/features/site_products/js/site_shop.min.js?v=1.0',
+  '/sites/all/modules/custom/features/site_search/js/site_search.min.js?v=1',
+  '/sites/all/libraries/bootstrap/js/bootstrap.min.js?v=3.3.6',
+  '/sites/all/themes/ntb/js/ntb.behaviors.min.js?v=1.0',
+  '/sites/all/modules/contrib/jquery_update/replace/jquery/1.10/jquery.min.js?v=1.10.2',
+  '/sites/all/modules/contrib/google_analytics/googleanalytics.js'
+  // '/sites/all/modules/custom/features/site_products/js/site_products.min.js?v=1.0'
 ];
 
 self.addEventListener('install', function (event) {
@@ -34,17 +36,42 @@ self.addEventListener('activate', function (event) {
   // Do activate stuff: This will come later on.
 });
 
-self.addEventListener('fetch', function (event) {
-  'use strict';
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function (response) {
+      .then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
