83d1ce0bc53a78d3dbb2c1aaf9612fd3521927ab
Gennaro Avolio-Toly <gennaroavolio-toly@Gennaros-MacBook-Pro.local>
2017-03-06 10:32:57 -0800

    finish sw update

--- sw.js
+++ sw.js
@@ -11,7 +11,11 @@
     '/shop/',
     '/ingredients/',
     '/about/',
-    '/shop/beam/'
+    '/shop/beam/',
+    '/shop/lid/',
+    '/shop/lip-cheek/',
+    '/shop/bath/',
+    '/shop/sets/'
   ];
 
   function updateStaticCache() {
@@ -34,8 +38,8 @@
         return cache.addAll([
           '/misc/jquery.once.js?v=1.2',
           '/misc/drupal.js',
-          '/misc/ajax.js?v=7.53',
-          '/misc/progress.js?v=7.53',
+          '/misc/ajax.js',
+          '/misc/progress.js',
           '/sites/all/libraries/modernizr/modernizr-2.8.3.min.js?v=2.3.8',
           '/sites/all/libraries/bootstrap/js/lib/collapse.min.js?v=3.3.6',
           '/sites/all/libraries/bootstrap/js/lib/dropdown.min.js?v=3.3.6',
@@ -60,52 +64,45 @@
       .then(function (cache) {
         cache.keys()
           .then(function (keys) {
-              if (keys.length > maxItems) {
-                cache.delete(keys[0])
-                  .then(trimCache(cacheName, maxItems));
-              }
+            if (keys.length > maxItems) {
+              cache.delete(keys[0])
+                .then(trimCache(cacheName, maxItems));
+            }
           });
       });
   }
 
   // Remove caches whose name is no longer valid
   function clearOldCaches() {
-    return caches.keys()
-      .then(function (cache) {
-        return Promise.all(keys
-          .filter(key => key.indexOf(version) !== 0)
-          .map(key => caches.delete(key))
-        );
-      });
-  }
-
-  function clearOldCaches() {
     return caches.keys().then(function (cache) {
-      return Promise.all(keys.filter(function (key) {
-        return key.indexOf(version) !== 0;
-      }).map(function (key) {
-        return caches.delete(key);
-      }));
+      return Promise.all(
+        keys.filter(function (key) {
+          return key.indexOf(version) !== 0;
+        }).map(function (key) {
+          return caches.delete(key);
+        })
+      );
     });
   }
 
   self.addEventListener('install', function (event) {
     event.waitUntil(updateStaticCache()
-      .then( function() {
-        self.skipWaiting()
-      });
+      .then(function () {
+        self.skipWaiting();
+      })
+    );
   });
 
   self.addEventListener('activate', function (event) {
     event.waitUntil(clearOldCaches()
-      .then( function () {
-        self.clients.claim()
-      });
+      .then(function () {
+        self.clients.claim();
+      })
     );
   });
 
   self.addEventListener('message', function (event) {
-    if (event.data.command == 'trimCaches') {
+    if (event.data.command === 'trimCaches') {
       trimCache(pagesCacheName, 35);
       trimCache(imagesCacheName, 20);
     }
@@ -135,15 +132,18 @@
             var copy = response.clone();
             if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
               stashInCache(staticCacheName, request, copy);
-            } else {
+            }
+            else {
               stashInCache(pagesCacheName, request, copy);
             }
             return response;
           })
-          .catch( () => {
+          .catch(function () {
             // CACHE or FALLBACK
             return caches.match(request)
-              .then( response => response || caches.match('/offline') );
+              .then(function (response) {
+                response || caches.match('/offline')
+              });
           })
       );
       return;
@@ -159,12 +159,12 @@
               // NETWORK
               // If the request is for an image, stash a copy of this image in the images cache
               if (request.headers.get('Accept').indexOf('image') !== -1) {
-                  var copy = response.clone();
-                  stashInCache(imagesCacheName, request, copy);
+                var copy = response.clone();
+                stashInCache(imagesCacheName, request, copy);
               }
               return response;
             })
-            .catch( function () {
+            .catch(function () {
               // OFFLINE
               // If the request is for an image, show an offline placeholder
               if (request.headers.get('Accept').indexOf('image') !== -1) {
@@ -174,78 +174,4 @@
         })
     );
   });
-
-
-  // var urlsToCache = [
-    // '/',
-    // '/sites/all/themes/ntb/logo.png',
-    // '/sites/all/themes/ntb/fonts/quicksand-regular-webfont.woff2',
-    // '/sites/all/themes/ntb/fonts/fontawesome-webfont.woff2?v=4.6.3',
-    // '/sites/all/themes/ntb/fonts/have_heart_one-webfont.woff2',
-    // '/sites/all/themes/ntb/fonts/quicksand-bold-webfont.woff2',
-    // '/misc/jquery.once.js?v=1.2',
-    // '/misc/drupal.js',
-    // '/misc/ajax.js?v=7.53',
-    // '/misc/progress.js?v=7.53',
-    // '/sites/all/modules/custom/features/site_products/js/site_shop.min.js?v=1.0',
-    // '/sites/all/modules/custom/features/site_search/js/site_search.min.js?v=1',
-    // '/sites/all/libraries/bootstrap/js/bootstrap.min.js?v=3.3.6',
-    // '/sites/all/themes/ntb/js/ntb.behaviors.min.js?v=1.0',
-    // '/sites/all/modules/custom/features/site_products/js/site_products.min.js?v=1.0',
-    // '/sites/all/modules/contrib/dc_ajax_add_cart/js/dc_ajax_add_cart_html.js',
-  // ];
-  //
-  // self.addEventListener('install', function (event) {
-  //   event.waitUntil(
-  //     caches.open(CACHE_NAME)
-  //       .then(function (cache) {
-  //         return cache.addAll(urlsToCache);
-  //       })
-  //   );
-  // });
-  //
-  // self.addEventListener('activate', function (event) {
-  //
-  //   // Do activate stuff: This will come later on.
-  // });
-  //
-  // self.addEventListener('fetch', function(event) {
-  //   event.respondWith(
-  //     caches.match(event.request)
-  //       .then(function(response) {
-  //         // Cache hit - return response
-  //         if (response) {
-  //           return response;
-  //         }
-  //
-  //         // IMPORTANT: Clone the request. A request is a stream and
-  //         // can only be consumed once. Since we are consuming this
-  //         // once by cache and once by the browser for fetch, we need
-  //         // to clone the response.
-  //         var fetchRequest = event.request.clone();
-  //
-  //         return fetch(fetchRequest).then(
-  //           function(response) {
-  //             // Check if we received a valid response
-  //             if(!response || response.status !== 200 || response.type !== 'basic') {
-  //               return response;
-  //             }
-  //
-  //             // IMPORTANT: Clone the response. A response is a stream
-  //             // and because we want the browser to consume the response
-  //             // as well as the cache consuming the response, we need
-  //             // to clone it so we have two streams.
-  //             var responseToCache = response.clone();
-  //
-  //             caches.open(CACHE_NAME)
-  //               .then(function(cache) {
-  //                 cache.put(event.request, responseToCache);
-  //               });
-  //
-  //             return response;
-  //           }
-  //         );
-  //       })
-  //     );
-  // });
 });
