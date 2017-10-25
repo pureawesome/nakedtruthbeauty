bc736a7e8cddde5c63e510d4ead9f2ffe94f174a
Gennaro Avolio-Toly <gennaroavolio-toly@Gennaros-MacBook-Pro-2.local>
2017-10-25 11:33:33 -0700

    rem units

--- sites/all/themes/ntb/template.php
+++ sites/all/themes/ntb/template.php
@@ -236,10 +236,13 @@ function ntb_library() {
         'defer' => TRUE,
         'scope' => 'footer',
       ),
-      'loadCSS("/' . drupal_get_path('theme', 'ntb') . '/css/ntb.css?v=' . CSS_VERSION . '");' => array(
-        'type' => 'inline',
-      ),
+      // 'loadCSS("/' . drupal_get_path('theme', 'ntb') . '/css/ntb.css?v=' . CSS_VERSION . '");' => array(
+      //   'type' => 'inline',
+      // ),
     ),
+    'css' => [
+      drupal_get_path('theme', 'ntb') . '/css/ntb.css' => [],
+    ],
     'dependencies' => [
       // ['ntb', 'bootstrap_collapse'],
       // ['ntb', 'bootstrap_dropdown'],
