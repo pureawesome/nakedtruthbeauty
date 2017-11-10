a0006da83622df522491bde4aa92907cfcc18a24
Gennaro Avolio-Toly <gennaro.the.younger@gmail.com>
2017-11-09 23:18:13 -0800

    settings tweak

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -626,13 +626,13 @@ drupal_fast_404();
  */
 # $conf['theme_debug'] = TRUE;
 # Set the $base_url parameter if we are running on Pantheon:
-if (PANTHEON_ENVIRONMENT == 'lando') {
+if (!defined('PANTHEON_ENVIRONMENT')) {
   $databases['default']['default'] = array(
     'driver' => 'mysql',
-    'database' => getenv('DB_NAME'),
+    'database' => 'nakedtruth',
     'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'host' => getenv('DB_HOST'),
+    'password' => getenv('DB_PASS'),
+    'host' => 'localhost',
     'prefix' => '',
     'collation' => 'utf8_general_ci',
   );
