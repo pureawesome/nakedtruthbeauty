2b7073c26832cecc3917e505ec39bc95c611eb8c
Gennaro Avolio-Toly <gennaroavolio-toly@Gennaros-MacBook-Pro-2.local>
2018-01-25 15:31:09 -0800

    redirect test

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -683,3 +683,35 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
     exit();
   }
 }
+
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+  if ($_ENV['PANTHEON_ENVIRONMENT'] == 'dev') {
+    $primary_domain = 'sandbox.nakedtruthbeauty.com';
+  }
+  else if ($_ENV['PANTHEON_ENVIRONMENT'] == 'test') {
+    $primary_domain = 'staging.nakedtruthbeauty.com';
+  }
+  // Redirect to https://$primary_domain in the Live environment
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+    /** Replace www.example.com with your registered domain name */
+    $primary_domain = 'nakedtruthbeauty.com';
+  }
+  else {
+    // Redirect to HTTPS on every Pantheon environment.
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  }
+
+  if ($_SERVER['HTTP_HOST'] != $primary_domain
+      || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+      || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+    # Name transaction "redirect" in New Relic for improved reporting (optional)
+    if (extension_loaded('newrelic')) {
+      newrelic_name_transaction("redirect");
+    }
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
