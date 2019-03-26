5e30bc26b3cfd017025dd049a75b1dd87ce46e80
Gennaro Avolio-Toly <gennaro.the.younger@gmail.com>
2019-03-25 22:48:59 -0700

    recent posts

---
+++ sites/all/themes/ntb/templates/node/node--blog_recent_teaser.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * Blog Teaser view mode node template.
+ */
+?>
+<div class="blog-recent-teaser <?php print $type; ?>">
+  <?php if (isset($image)): ?>
+    <div class="img">
+      <?php print l(render($image), $node_url, array('html' => TRUE)); ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content">
+    <?php if (isset($title)): ?>
+      <div class="title">
+        <h3>
+          <?php print l($title, $node_url, array('html' => TRUE)); ?>
+        </h3>
+      </div>
+    <?php endif; ?>
+    <div class="description">
+      <?php print render($text); ?>
+      <span class="read-more"><?php print l(t('Read More'), $node_url); ?></span>
+    </div>
+  </div>
+</div>
